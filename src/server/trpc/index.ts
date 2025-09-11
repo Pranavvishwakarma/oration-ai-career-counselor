7d2330a4fb889488e7a059302eb2fdb18abf47c2
Pranavvishwakarma <pranav213234@gmail.com>
2025-09-11 10:13:57 +0530

    Fix tRPC transformer configuration

--- src/server/trpc/index.ts
+++ src/server/trpc/index.ts
@@ -1,8 +1,11 @@
 import { initTRPC } from '@trpc/server';
+import superjson from 'superjson';
 import { createContext, type Context } from './context';
 import { chatRouter } from './chat';
 
-const t = initTRPC.context<Context>().create();
+const t = initTRPC.context<Context>().create({
+  transformer: superjson,
+});
 
 export const appRouter = t.router({
   chat: chatRouter,
