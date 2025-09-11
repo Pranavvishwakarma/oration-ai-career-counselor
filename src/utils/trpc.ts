c52238297bd9a675753e932c78475ac4d507c135
Pranav Vishwakarma <103947396+Pranavvishwakarma@users.noreply.github.com>
2025-09-11 10:37:45 +0530

    Update trpc.ts

--- src/utils/trpc.ts
+++ src/utils/trpc.ts
@@ -9,7 +9,7 @@ const getBaseUrl = () => {
   }
   if (process.env.VERCEL_URL) return `https://${process.env.VERCEL_URL}`;
   if (process.env.NODE_ENV === 'production') return 'https://oration-ai-career-counselor.vercel.app';
-  return 'https://oration-ai-career-counselor.vercel.app';
+  return 'http://localhost:3000';
 };
 
 export const trpc = createTRPCReact<AppRouter>();
